6c179eb362f72bfeccd1c015ad311651614ae4f5
Anni-spb <122387985+Anni-spb@users.noreply.github.com>
2025-12-10 01:56:57 +0300

    Делает сетку главной страницы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,8 @@
-/* в этот файл добавляет скрипты*/
+document.querySelector('.nav').classList.remove('nav--nojs');
+const menuToggle = document.querySelector('.nav__toggle');
+const navList = document.querySelector('.nav__list');
+
+menuToggle.addEventListener ('click', () => {
+  navList.classList.toggle('nav__list--opened');
+  menuToggle.classList.toggle('nav__toggle--active');
+});
