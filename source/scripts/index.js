b2f77f18ee38f96aff2b7fc7cf757898c15ad1c7
Anni-spb <122387985+Anni-spb@users.noreply.github.com>
2025-12-18 16:56:48 +0300

    Делает сетку страницы каталога (#5)
    * Делает сетку страницы каталога
    
    * Правит отображение без Js
    
    * Добавляет карту

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -6,3 +6,65 @@ menuToggle.addEventListener ('click', () => {
   navList.classList.toggle('nav__list--opened');
   menuToggle.classList.toggle('nav__toggle--active');
 });
+
+/* КАРТА */
+const getCenter = () => {
+  if (window.innerWidth < 1280) {
+    return [59.938631, 30.323037];
+  } else {
+    return [59.938565, 30.317988];
+  }
+};
+
+const placemarkImageSet = (placemark) => {
+  if (window.innerWidth < 768) {
+    placemark.options.set({
+      iconImageHref: 'images/maps/map-pin-mobile@1x.png',
+      iconImageSize: [56, 52],
+      iconImageOffset: [-30, -37]
+    });
+  } else if ((window.devicePixelRatio >= 2) & (window.innerWidth < 768)) {
+    placemark.options.set({
+      iconImageHref: 'images/maps/map-pin-mobile@2x.png',
+      iconImageSize: [56, 52],
+      iconImageOffset: [-30, -37]
+    });
+  } else if ((window.devicePixelRatio >= 2) & (window.innerWidth >= 768)) {
+    placemark.options.set({
+      iconImageHref: 'images/maps/map-pin-tablet@2x.png',
+      iconImageSize: [113, 106],
+      iconImageOffset: [-60, -90]
+    });
+  } else {
+    placemark.options.set({
+      iconImageHref: 'images/maps/map-pin-tablet@1x.png',
+      iconImageSize: [113, 106],
+      iconImageOffset: [-60, -90]
+    });
+  }
+};
+
+const ymaps = window.ymaps;
+ymaps.ready(() => {
+  const center = getCenter();
+
+  const map = new ymaps.Map('map', {
+    center: center,
+    zoom: 16,
+    controls: ['zoomControl']
+  });
+
+  const placemark = new ymaps.Placemark([59.938631, 30.323037], {}, {
+    iconLayout: 'default#image',
+  });
+  placemarkImageSet(placemark);
+  map.geoObjects.add(placemark);
+
+  window.addEventListener('resize', () => {
+    const newCenter = getCenter();
+    map.setCenter(newCenter);
+
+    placemark.geometry.setCoordinates([59.938631, 30.323037]);
+    placemarkImageSet(placemark);
+  });
+});
